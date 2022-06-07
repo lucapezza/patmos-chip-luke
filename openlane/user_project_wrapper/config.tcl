4adece66df76983816ef4a7d83b35cac87acef9a
tjarker <tjark-petersen@gmx.de>
2022-06-07 12:03:32 +0000

    remove PDN core ring

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -41,12 +41,16 @@ set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/HelloMorse.v"
 
 ## Clock configurations
-set ::env(CLOCK_PORT) "user_clock2"
-set ::env(CLOCK_NET) "patmos.clock"
+set ::env(CLOCK_PORT) "wb_clk_i"
+set ::env(CLOCK_NET) "patmos.clock morse.clock"
 
 set ::env(CLOCK_PERIOD) "100"
 
 set ::env(DESIGN_IS_CORE) 1
+
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 2ns
+set ::env(GLB_RESIZER_MAX_SLEW_MARGIN) 20
+set ::env(GLB_RESIZER_MAX_CAP_MARGIN) 20
 
 ## Internal Macros
 ### Macro PDN Connections
@@ -54,7 +58,7 @@ set ::env(FP_PDN_CHECK_NODES) 0
 set ::env(FP_SIZING) absolute
 set ::env(FP_PDN_MACRO_HOOKS) "patmos.patmos.Spm.sky130_sram_1kbyte_1rw1r_32x256_8 vccd1 vssd1 \
  patmos.patmos.cores_0.fetch.bootMem.memWithWrEven vccd1 vssd1 \
- patmos.patmos.cores_0.fetch.bootMem.memWithWrOdd vccd1 "
+ patmos.patmos.cores_0.fetch.bootMem.memWithWrOdd vccd1 vssd1"
 
 ### Macro Placement
 set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
@@ -83,6 +87,14 @@ set ::env(MACRO_PLACE_CHANNEL) "50 50"
 
 set ::env(SYNTH_USE_PG_PINS_DEFINES) "USE_POWER_PINS"
 
+set ::env(RUN_MAGIC_DRC) 0
+set ::env(RUN_KLAYOUT_DRC) 0
+set ::env(MAGIC_DRC_USE_GDS) 0
+set ::env(RUN_KLAYOUT_DRC) 0
+set ::env(RUN_KLAYOUT_XOR) 0
+# If you're going to use multiple power domains, then disable cvc run.
+set ::env(RUN_CVC) 1
+
 # set ::env(GLB_RT_MAXLAYER) 5
 set ::env(RT_MAX_LAYER) {met4}
 
@@ -100,26 +112,63 @@ set ::env(FP_PDN_CHECK_NODES) 0
 #set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
 
+set ::env(FP_PDN_CORE_RING) 0
 set ::env(FP_PDN_ENABLE_RAILS) 1
 
+
 # You can draw more power domains if you need to 
-set ::env(VDD_NETS) [list {vccd1}]
-set ::env(GND_NETS) [list {vssd1}]
+set ::env(VDD_NETS) {vccd1 vccd2 vdda1 vdda2}
+set ::env(GND_NETS) {vssd1 vssd2 vssa1 vssa2}
+#
+set ::env(VDD_PIN) {vccd1}
+set ::env(GND_PIN) {vssd1}
+
 
 set ::env(DIODE_INSERTION_STRATEGY) 4 
-# If you're going to use multiple power domains, then disable cvc run.
-set ::env(RUN_CVC) 1
+
 
 set ::env(ROUTING_CORES) 4
 
-set ::env(RUN_KLAYOUT_DRC) 0
-set ::env(RUN_KLAYOUT_XOR) 0
+
 
 set ::env(LVS_CONNECT_BY_LABEL) 1
 
-set ::env(MAGIC_DRC_USE_GDS) 0
 
 set ::env(FP_PDN_ENABLE_MACROS_GRID) 1
 set ::env(FP_PDN_VOFFSET) 8.16
 set ::env(FP_PDN_HOFFSET) 8.32
 set ::env(FP_PDN_AUTO_ADJUST) 0
+
+set ::env(QUIT_ON_LVS_ERROR) "0"
+set ::env(QUIT_ON_MAGIC_DRC) "0"
+set ::env(QUIT_ON_NEGATIVE_WNS) "0"
+set ::env(QUIT_ON_SLEW_VIOLATIONS) "0"
+set ::env(QUIT_ON_TIMING_VIOLATIONS) "0"
+
+
+
+
+set ::env(FP_PDN_IRDROP) "0"
+set ::env(FP_PDN_HORIZONTAL_HALO) "10"
+set ::env(FP_PDN_VERTICAL_HALO) "10"
+
+#
+
+set ::env(FP_PDN_CORE_RING_HOFFSET) {12.45}
+set ::env(FP_PDN_CORE_RING_HSPACING) {1.7}
+set ::env(FP_PDN_CORE_RING_HWIDTH) {3.1}
+
+set ::env(FP_PDN_CORE_RING_VOFFSET) {12.45}
+set ::env(FP_PDN_CORE_RING_VSPACING) {1.7}
+set ::env(FP_PDN_CORE_RING_VWIDTH) {3.1}
+
+
+set ::env(FP_PDN_VOFFSET) "5"
+set ::env(FP_PDN_VPITCH) "80"
+set ::env(FP_PDN_VSPACING) "15.5"
+set ::env(FP_PDN_VWIDTH) "3.1"
+
+set ::env(FP_PDN_HOFFSET) "10"
+set ::env(FP_PDN_HPITCH) "90"
+set ::env(FP_PDN_HSPACING) "10"
+set ::env(FP_PDN_HWIDTH) "3.1"
